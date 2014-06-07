e4ab68d5c1fcd6b38ff8c222a1575a7e2562850a
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2014-06-07 13:10:57 -0500

    showme

---
+++ application/controllers/admin.php
@@ -0,0 +1,508 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->load->database();
+		
+		//Helpers
+		$this->load->helper('url');
+		$this->load->helper('date');
+die("ok");
+		$this->load->library('grocery_CRUD');
+		
+		
+		die("ok");
+		session_start();
+	}
+
+	/*Salida de las vistas*/
+	public function _example_output($output = null) {
+		$this->load->view('admin.php', $output);
+	}
+	
+	/*Users metodo para verificar si es usuario*/
+	private function isUser($redirect = true, $admin = false) {
+		if(isset($_SESSION['user_id'])) {
+			$user_id = $_SESSION['user_id'];
+			
+			$this->load->model('showme_model');
+			$user = $this->showme_model->getUser($_SESSION['user_id']);
+			
+			if($user) {
+				
+				return $user;
+				
+				if($redirect) {
+					header('Location: ' . site_url('admin/facturas'));
+				}
+				
+				return $user;
+			} else {
+				if($redirect) {
+					header('Location: ' . site_url('admin/login'));
+				}
+				
+				return false;
+			}
+		} else {
+			if($redirect) {
+				header('Location: ' . site_url('admin/login'));
+			}
+			
+			return false;
+		}
+	}
+	
+	/*Metodo para hacer login*/
+	public function login() {
+		if($this->isUser(false)) {
+			header('Location: ' . site_url('admin/facturas'));
+		} else {
+			$vars["error"] = false;
+			
+			if(isset($_POST["submit"])) {
+				$this->load->model('showme_model');
+				$user = $this->showme_model->isUser(trim(str_replace("'","",$_POST["email"])), md5($_POST["pwd"]));
+				
+				if($user) {
+					$_SESSION['user_id'] = $user->id_user;
+					$_SESSION['email']   = $user->email;
+					
+					header('Location: ' . site_url('admin/facturas'));
+				}
+				
+				$vars["error"] = "datos incorrectos";
+			}
+			
+			$this->load->view('login.php', $vars);
+		}
+	}
+	
+	/*Metodo para cerrar session*/
+	public function logout() {
+		session_unset(); 
+		session_destroy();
+		
+		header('Location: ' . site_url('admin/login'));
+	}
+	
+	
+	/*Metodo de denuncias*/
+	public function facturas() {
+		$user = $this->isUser();
+		$crud = new grocery_CRUD();
+		
+		/*Tabla, Título y Orden*/
+		$crud->set_theme('datatables');
+		$crud->set_table('facturas');
+		$crud->set_subject('Facturas');
+		
+		/*Relaciones n_n*/
+		
+		$output = $crud->render();
+		$this->_example_output($output);
+	}
+	
+	/*Nombres en español de los campos*/
+	public function display_as_denuncias($crud) {
+		/*Lugar denuncia*/
+		$crud->display_as('fecha_creada', 'Fecha que se recibió la queja');
+		$crud->display_as('id_denuncia', 'Número de queja');
+		
+		$crud->display_as('id_lugar_denuncia', 'Lugar de denuncia');
+		$crud->set_relation('id_lugar_denuncia', 'lugares_denuncia', 'nombre');
+		/*Lugar denuncia*/
+		$crud->display_as('id_tipo_queja', 'Tipo de queja');
+		$crud->set_relation('id_tipo_queja', 'tipos_quejas', 'nombre');
+		
+		/*Datos sobre cruce de la frontera*/
+		$crud->display_as('intentos', 'Cuántas veces has intentado cruzar la frontera');
+		
+		/*Falta de trabajo, Violencia/Seguridad, Ramificación familiar, Otro*/
+		$crud->display_as('motivo_migracion', 'Cuál es el motivo de migración');
+		$crud->field_type('motivo_migracion', 'dropdown', array('Falta de trabajo' => 'Falta de trabajo', 'Violencia/Seguridad' => 'Violencia/Seguridad', 'Ramificación familiar' => 'Ramificación familiar', 'Otro' => 'Otro'));
+		
+		$crud->field_type('viaja_solo', 'dropdown', array(1 => 'Si', 2 => 'No'));
+		$crud->display_as('con_quien_viaja', 'Con quien viaja');
+		/*Lugar deportado*/
+		$crud->field_type('deportado', 'dropdown', array(1 => 'Si', 2 => 'No'));
+		$crud->display_as('momento_deportado', 'Donde fue deportado');
+		$crud->field_type('momento_deportado', 'dropdown', array('Al cruzar la frontera' => 'Al cruzar la frontera', 'Vivías en USA' => 'Vivías en USA', 'Otro' => 'Otro'));
+		/*Separacion familiar*/
+		$crud->display_as('separacion_familiar', 'Te separaron de algún familiar');
+		$crud->field_type('separacion_familiar', 'dropdown', array(1 => 'Si', 2 => 'No'));
+		$crud->display_as('familiar_separado', 'Que familiar');
+		$crud->display_as('situacion_familiar', 'Sabes que paso con tu familiar');
+		$crud->display_as('acto_siguiente', 'Qué piensa hacer ahora');
+		
+		/*Coyote*/
+		$crud->display_as('coyote_guia', 'Contacto al coyote o guía que lo pasaría');
+		$crud->field_type('coyote_guia', 'dropdown', array(1 => 'Si', 2 => 'No'));
+		$crud->display_as('monto_coyote', 'Cuanto le cobraría');
+		$crud->display_as('paquete_pago', 'Que incluía el pago');
+		
+		$crud->display_as('conocimineto_punto_fronterizo', 'Porque punto fronterizo cruzaste o vas a cruzar a USA');
+		$crud->field_type('conocimineto_punto_fronterizo', 'dropdown', array(1 => 'Si', 2 => 'No'));
+		
+		$crud->display_as('nombre_punto_fronterizo', 'Porque punto fronterizo cruzaste o vas a cruzar a USA');
+		$crud->display_as('lugar_de_usa', 'A que lugar de USA vas');
+		
+		/*Antecedentes de autoridades*/
+		$crud->display_as('autoridades_viaje', 'Durante el viaje con que autoridades te encontraste');
+		$crud->display_as('dano_autoridad', 'Alguna de las autoridades te causaron daño');
+		$crud->field_type('dano_autoridad', 'dropdown', array(1 => 'Si', 2 => 'No'));
+		$crud->display_as('id_autoridad_dano', 'Que autoridad lo hizo');
+		$crud->set_relation('id_autoridad_dano', 'autoridades', 'nombre');
+		
+		/*Hechos violatorios a derechos humanos*/
+		$crud->display_as('fecha_injusticia', 'Cuándo se cometió la injusticia');
+		$crud->display_as('id_pais_injusticia', 'País donde se cometió la injusticia');
+		$crud->set_relation('id_pais_injusticia', 'paises', 'nombre');
+		$crud->display_as('id_estado_injusticia', 'Estado donde se cometió la injusticia');
+		$crud->set_relation('id_estado_injusticia', 'estados', 'nombre');
+		$crud->display_as('municipio_injusticia', 'Municipio donde se cometió la injusticia');
+		$crud->display_as('espacio_fisico_injusticia', 'Espacio físico de la injusticia');
+		$crud->display_as('detonante_injusticia', 'Situación que detona la injusticia');
+		$crud->display_as('numero_migrantes_injusticia', 'Número de migrantes que habia con usted cuando se cometio el abuso');
+		$crud->display_as('id_transporte_viaje_injusticia', 'En que viajaba');
+		$crud->set_relation('id_transporte_viaje_injusticia', 'transportes', 'nombre');
+		$crud->display_as('lugar_abordaje_transporte', 'Donde abordo el transporte');
+		$crud->display_as('destino_transporte', 'Destino del transporte');
+		
+		/*Datos de la autoridad responsable*/
+		$crud->display_as('autoridades_responables', 'Nombre de las instituciones involucradas');
+		$crud->display_as('numero_oficiales_responsables', 'Número de oficiales responsables');
+		$crud->display_as('algun_nombre_responsables', 'Escucho o sabe algún nombre de  los oficiales involucrados');
+		$crud->display_as('carcteristicas_ficias_policia_responsable', 'Características fìsicas');
+		$crud->display_as('apodos_responsables', 'Escucho o sabe algún apodo de  los oficiales involucrados');
+		
+		$crud->display_as('uniformado_responsables', 'Los responsables estaban uniformados');
+		$crud->field_type('uniformado_responsables', 'dropdown', array('Si' => 'Si', 'No' => 'No', 'No vio' => 'No vio'));
+		
+		$crud->display_as('color_uniforme_responsables', 'Color de uniforme de oficiales responsables');
+		$crud->display_as('insignias_responsables', 'Insignias de uniforme de oficiales responsables');
+		
+		$crud->display_as('responsables_abordo_vehiculos_responsables', 'Iban a bordo de Vehículos');
+		$crud->field_type('responsables_abordo_vehiculos_responsables', 'dropdown', array('Si' => 'Si', 'No' => 'No'));
+		
+		$crud->display_as('id_tipo_transporte_responsables', 'Tipo de vehículo');
+		$crud->display_as('numero_vehiculos_responsables', 'Número de vehículos');
+		$crud->set_relation('id_tipo_transporte_responsables', 'transportes', 'nombre');
+		$crud->display_as('placas_vehiculos_responsables', 'Placas');
+		
+		/*descipción del evento*/
+		$crud->display_as('descripcion_evento', 'Descipción del evento');
+		
+		/*Monto de la extorsion*/
+		$crud->display_as('monto_extorsion', 'En caso de extorsión mencione el monto');
+		
+		/*Derechos violados*/
+		
+		/*Estado actual del caso*/
+		$crud->display_as('id_estado_caso', 'Estado actual del caso');
+		$crud->set_relation('id_estado_caso', 'etados_casos', 'nombre');
+		
+		$crud->display_as('estado_seguimiento', 'Seguimiento');
+		$crud->field_type('estado_seguimiento', 'dropdown', array('Defensa' => 'Defensa', 'Canalización a una instancia' => 'Canalización a una instancia'));
+		
+		$crud->display_as('notas_seguimiento', 'Notas sobre el seguimiento');
+		$crud->display_as('nombre_persona_atendio_seguimiento', 'Nombre de la persona que atendio el caso');
+		$crud->display_as('telefono_seguimiento', 'Teléfono de contacto para seguimiento');
+		$crud->display_as('documento1_seguimiento', 'Documento adicional 1');
+		$crud->display_as('documento2_seguimiento', 'Documento adicional 2');
+		$crud->display_as('documento3_seguimiento', 'Documento adicional 3');
+		$crud->display_as('documento4_seguimiento', 'Documento adicional 4');
+		$crud->display_as('documento5_seguimiento', 'Documento adicional 5');
+		
+		$crud->display_as('documento6_seguimiento', 'Multimedia adicional 1');
+		$crud->display_as('documento7_seguimiento', 'Multimedia adicional 2');
+		$crud->display_as('documento8_seguimiento', 'Multimedia adicional 3');
+		$crud->display_as('documento9_seguimiento', 'Multimedia adicional 4');
+		$crud->display_as('documento10_seguimiento', 'Multimedia adicional 5');
+		
+		$crud->set_field_upload('documento1_seguimiento', 'assets/uploads/files');
+		$crud->set_field_upload('documento2_seguimiento', 'assets/uploads/files');
+		$crud->set_field_upload('documento3_seguimiento', 'assets/uploads/files');
+		$crud->set_field_upload('documento4_seguimiento', 'assets/uploads/files');
+		$crud->set_field_upload('documento5_seguimiento', 'assets/uploads/files');
+		$crud->set_field_upload('documento6_seguimiento', 'assets/uploads/files');
+		$crud->set_field_upload('documento7_seguimiento', 'assets/uploads/files');
+		$crud->set_field_upload('documento8_seguimiento', 'assets/uploads/files');
+		$crud->set_field_upload('documento9_seguimiento', 'assets/uploads/files');
+		$crud->set_field_upload('documento10_seguimiento', 'assets/uploads/files');
+		return true;
+	}
+	
+	/*Metodo de migrantes*/
+	public function migrantes() {
+		$user = $this->isUser();
+		$crud = new grocery_CRUD();
+		
+		/*Tabla y título*/
+		$crud->set_theme('datatables');
+		$crud->set_table('migrantes');
+		$crud->set_subject('Migrantes');
+		
+		/*Columnas(Vista), campos y campos obligatorios*/
+		$crud->columns('id_migrante', 'nombre', 'id_pais', 'id_estado', 'municipio', 'edad');
+		$crud->required_fields('nombre');
+		
+		
+		/*Relaciones con tablas*/
+		/*Pais*/
+		$crud->display_as('id_migrante', 'ID');
+		$crud->display_as('id_pais', 'País');
+		$crud->set_relation('id_pais', 'paises', 'nombre');
+		/*Estado*/
+		$crud->display_as('id_estado', 'Estado/Departamento');
+		$crud->set_relation('id_estado', 'estados', 'nombre');
+		/*Genero*/
+		$crud->display_as('id_genero', 'Género');
+		$crud->set_relation('id_genero', 'generos', 'nombre');
+		/*Estado civil*/
+		$crud->display_as('id_estado_civil', 'Estado Civil');
+		$crud->set_relation('id_estado_civil', 'estado_civil', 'nombre');
+		/*Escolaridad*/
+		$crud->display_as('escolaridad', 'Escolaridad');
+		$crud->field_type('escolaridad', 'dropdown', array(
+			'Sin instrucción' => 'Sin instrucción',
+			'Primaria' => 'Primaria',
+			'Primaria inconclusa' => 'Primaria inconclusa',
+			'Secundaria' => 'Secundaria',
+			'Secundaria inconclusa' => 'Secundaria inconclusa',
+			'Preparatoria' => 'Preparatoria',
+			'Preparatoria inconclusa' => 'Preparatoria inconclusa',
+			'Licenciatura' => 'Licenciatura',
+			'Licenciatura inconlusa' => 'Licenciatura inconlusa',
+			'Maestria' => 'Maestria',
+			'Doctorado' => 'Doctorado'
+		));
+			
+		/*Pueblo indigena*/
+		$crud->display_as('pueblo_indigena', 'Pertenece a algún pueblo indígena');
+		$crud->field_type('pueblo_indigena', 'dropdown', array(1 => 'Si', 2 => 'No'));
+		/*Español*/
+		$crud->display_as('espanol', 'Dominio del español');
+		$crud->field_type('espanol', 'dropdown', array(1 => 'Si', 2 => 'No'));
+		
+		$output = $crud->render();
+		
+		$this->_example_output($output);
+	}
+	
+	/*Autoridades*/
+	public function autoridades() {
+		$user = $this->isUser();
+		$crud = new grocery_CRUD();
+		
+		/*Tabla y título*/
+		$crud->set_theme('datatables');
+		$crud->set_table('autoridades');
+		$crud->set_subject('Autoridades');
+		
+		/*Columnas(Vista), campos y campos obligatorios*/
+		$crud->columns('id_autoridad', 'nombre');
+		$crud->required_fields('nombre');
+		
+		$output = $crud->render();
+		
+		$this->_example_output($output);
+	}
+	
+	/*Derechos*/
+	public function derechos() {
+		$user = $this->isUser();
+		$crud = new grocery_CRUD();
+		
+		/*Tabla y título*/
+		$crud->set_theme('datatables');
+		$crud->set_table('derechos');
+		$crud->set_subject('Derechos');
+		
+		/*Columnas(Vista), campos y campos obligatorios*/
+		$crud->columns('id_derecho', 'nombre');
+		$crud->required_fields('nombre');
+		
+		$output = $crud->render();
+		
+		$this->_example_output($output);
+	}
+	
+	/*Violaciones a los Derechos*/
+	public function violaciones_derechos() {
+		$user = $this->isUser();
+		$crud = new grocery_CRUD();
+		
+		/*Tabla y título*/
+		$crud->set_theme('datatables');
+		$crud->set_table('violacion_derechos');
+		$crud->set_subject('Violaciónes a derechos');
+		
+		/*Columnas(Vista), campos y campos obligatorios*/
+		$crud->columns('id_violacion', 'id_derecho', 'nombre');
+		$crud->required_fields('id_derecho', 'nombre');
+		
+		/*Relaciones con tablas*/
+		/*Derechos*/
+		$crud->display_as('nombre', 'Violación a derecho');
+		$crud->display_as('id_derecho', 'Derecho');
+		$crud->set_relation('id_derecho', 'derechos', 'nombre');
+		
+		$output = $crud->render();
+		
+		$this->_example_output($output);
+	}
+	
+	/*Paises*/
+	public function paises() {
+		$user = $this->isUser();
+		$crud = new grocery_CRUD();
+		
+		/*Tabla y título*/
+		$crud->set_theme('datatables');
+		$crud->set_table('paises');
+		$crud->set_subject('Paises');
+		
+		/*Columnas(Vista), campos y campos obligatorios*/
+		$crud->columns('id_pais', 'nombre');
+		$crud->required_fields('nombre');
+
+		$output = $crud->render();
+		
+		$this->_example_output($output);
+	}
+	
+	/*Estados*/
+	public function estados() {
+		$user = $this->isUser();
+		$crud = new grocery_CRUD();
+		
+		/*Tabla y título*/
+		$crud->set_theme('datatables');
+		$crud->set_table('estados');
+		$crud->set_subject('Estados/Departamentos');
+		
+		/*Columnas(Vista), campos y campos obligatorios*/
+		$crud->columns('id_estado', 'id_pais', 'nombre');
+		$crud->required_fields('id_pais', 'nombre');
+
+		/*Relaciones con tablas*/
+		/*Estados*/
+		$crud->display_as('id_pais', 'Pais');
+		$crud->set_relation('id_pais', 'paises', 'nombre');
+		
+		$output = $crud->render();
+		
+		$this->_example_output($output);
+	}
+	
+	/*Estado de los casos/denuncias*/
+	public function estados_casos() {
+		$user = $this->isUser();
+		$crud = new grocery_CRUD();
+		
+		/*Tabla y título*/
+		$crud->set_theme('datatables');
+		$crud->set_table('etados_casos');
+		$crud->set_subject('Estado de los casos/denuncias');
+		
+		/*Columnas(Vista), campos y campos obligatorios*/
+		$crud->columns('id_estado_caso', 'nombre');
+		$crud->required_fields('nombre');
+		
+		$output = $crud->render();
+		
+		$this->_example_output($output);
+	}
+	
+	/*Lugares de las denuncias*/
+	public function lugares_denuncia() {
+		$user = $this->isUser();
+		$crud = new grocery_CRUD();
+		
+		/*Tabla y título*/
+		$crud->set_theme('datatables');
+		$crud->set_table('lugares_denuncia');
+		$crud->set_subject('Lugares de las denuncias');
+		
+		/*Columnas(Vista), campos y campos obligatorios*/
+		$crud->columns('id_lugar_denuncia', 'nombre');
+		$crud->required_fields('nombre');
+		
+		$output = $crud->render();
+		
+		$this->_example_output($output);
+	}
+	
+	/*Que incluia el pago*/
+	public function paquete_pago() {
+		$user = $this->isUser();
+		$crud = new grocery_CRUD();
+		
+		/*Tabla y título*/
+		$crud->set_theme('datatables');
+		$crud->set_table('paquete_pago');
+		$crud->set_subject('Que incluia el pago');
+		
+		/*Columnas(Vista), campos y campos obligatorios*/
+		$crud->columns('id_paquete', 'nombre');
+		$crud->required_fields('nombre');
+		
+		$output = $crud->render();
+		
+		$this->_example_output($output);
+	}
+	
+	/*Tipo de quejas*/
+	public function tipos_quejas() {
+		$user = $this->isUser();
+		$crud = new grocery_CRUD();
+		
+		/*Tabla y título*/
+		$crud->set_theme('datatables');
+		$crud->set_table('tipos_quejas');
+		$crud->set_subject('Tipo de quejas');
+		
+		/*Columnas(Vista), campos y campos obligatorios*/
+		$crud->columns('id_tipo_queja', 'nombre');
+		$crud->required_fields('nombre');
+		
+		$output = $crud->render();
+		
+		$this->_example_output($output);
+	}
+	
+	/*Transportes*/
+	public function transportes() {
+		$user = $this->isUser();
+		$crud = new grocery_CRUD();
+		
+		/*Tabla y título*/
+		$crud->set_theme('datatables');
+		$crud->set_table('transportes');
+		$crud->set_subject('Transportes');
+		
+		/*Columnas(Vista), campos y campos obligatorios*/
+		$crud->columns('id_transporte', 'nombre');
+		$crud->required_fields('nombre');
+		
+		$output = $crud->render();
+		
+		$this->_example_output($output);
+	}
+	
+	/*obtener nombre unico de un campo*/
+	function unique_field_name($field_name) {
+		return 's'.substr(md5($field_name),0,8); //This s is because is better for a string to begin with a letter and not with a number
+    }
+	
+	/*metodo index - redirect a denuncias*/
+	public function index() {
+		header('Location: ' . site_url('admin/facturas'));
+		
+		return false;
+	}
+}
---
+++ application/views/admin.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>¿Quién compró? Plataforma web de información sobre el uso del dinero público en el Congreso de México</title>
+<?php 
+foreach($css_files as $file): ?>
+	<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+<?php endforeach; ?>
+<?php foreach($js_files as $file): ?>
+	<script src="<?php echo $file; ?>"></script>
+<?php endforeach; ?>
+<style type='text/css'>
+body
+{
+	font-family: Arial;
+	font-size: 14px;
+}
+a {
+    color: blue;
+    text-decoration: none;
+    font-size: 14px;
+}
+a:hover
+{
+	text-decoration: underline;
+}
+strong { font-size:16px; }
+.link { cursor:pointer; color:blue; font-size:14px; }
+#catalogos { display:none; padding:0;}
+</style>
+</head>
+<body>
+	<div>
+		<a onclick="javascript: return confirmacion('<?php echo site_url('admin/migrantes')?>')" href="javascript:void(0)">
+			<?php if($this->uri->segment(2) == "migrantes") { ?><strong>Migrantes</strong><?php } else { ?>Migrantes<?php } ?>
+		</a> |
+		<a onclick="javascript: return confirmacion('<?php echo site_url('admin/denuncias')?>')" href="javascript:void(0)">
+			<?php if($this->uri->segment(2) == "denuncias") { ?><strong>Denuncias</strong><?php } else { ?>Denuncias<?php } ?>
+		</a> |
+		
+		<?php if(isset($_SESSION['user_id'])) { ?>
+			<a onclick="javascript: return confirmacion('<?php echo site_url('admin/logout')?>')" href="javascript:void(0)">Cerrar sesión</a> | 
+		<?php } ?>
+		
+		<span class="link" id="ver-catalogos">Mostrar/Ocultar Catalogos</span>
+		
+		<span id="catalogos" class="hide">
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<a onclick="javascript: return confirmacion('<?php echo site_url('admin/estados')?>')" href="javascript:void(0)">
+				<?php if($this->uri->segment(2) == "estados") { ?><strong>Estados/Departamentos</strong><?php } else { ?>Estados/Departamentos<?php } ?>
+			</a> |
+			<a onclick="javascript: return confirmacion('<?php echo site_url('admin/autoridades')?>')" href="javascript:void(0)">
+				<?php if($this->uri->segment(2) == "autoridades") { ?><strong>Autoridades</strong><?php } else { ?>Autoridades<?php } ?>
+			</a> |
+			<a onclick="javascript: return confirmacion('<?php echo site_url('admin/paises')?>')" href="javascript:void(0)">
+				<?php if($this->uri->segment(2) == "paises") { ?><strong>Paises</strong><?php } else { ?>Paises<?php } ?>
+			</a> |
+			<a onclick="javascript: return confirmacion('<?php echo site_url('admin/estados_casos')?>')" href="javascript:void(0)">
+				<?php if($this->uri->segment(2) == "estados_casos") { ?><strong>Estado de los casos</strong><?php } else { ?>Estado de los casos<?php } ?>
+			</a> |
+			<a onclick="javascript: return confirmacion('<?php echo site_url('admin/transportes')?>')" href="javascript:void(0)">
+				<?php if($this->uri->segment(2) == "transportes") { ?><strong>Transportes</strong><?php } else { ?>Transportes<?php } ?>
+			</a>
+		</span>
+	</div>
+	
+	
+		
+	<div style='height:20px;'></div>  
+    <div>
+		<?php echo $output; ?>
+    </div>
+    <script type="text/javascript">
+		function confirmacion(url) {
+			if(confirm('¿Esta segura/o que desea salir (verifique que ha guardado la información)?')) {
+				document.location = url;
+			} else {
+				return false;
+			}
+		}
+		
+		$(document).ready( function () {
+			$("#ver-catalogos").click( function () {
+				$("#catalogos").toggle();
+			});
+			
+			<?php if($this->uri->segment(3) != "read") { ?>
+				/*Guia-Coyote*/
+				$("#monto_coyote_field_box").css("margin-left", "50px");
+				$("#paquete_pago_field_box").css("margin-left", "50px");
+				$("#monto_coyote_field_box").hide();
+				$("#paquete_pago_field_box").hide();
+				
+				$("#field-coyote_guia").change( function () {
+					if($("#field-coyote_guia").val() == 1) {
+						$("#monto_coyote_field_box").show();
+						$("#paquete_pago_field_box").show();
+					} else {
+						$("#monto_coyote_field_box").hide();
+						$("#paquete_pago_field_box").hide();
+					}
+				});
+				
+				/*Viajaba solo*/
+				$("#con_quien_viaja_field_box").css("margin-left", "50px");
+				$("#con_quien_viaja_field_box").hide();
+		
+				$("#field-viaja_solo").change( function () {
+					if($("#field-viaja_solo").val() == 2) {
+						$("#con_quien_viaja_field_box").show();
+						$("#con_quien_viaja_field_box").show();
+					} else {
+						$("#con_quien_viaja_field_box").hide();
+						$("#con_quien_viaja_field_box").hide();
+					}
+				});
+				
+				/*Momento de deportado*/
+				$("#momento_deportado_field_box").css("margin-left", "50px");
+				$("#momento_deportado_field_box").hide();
+		
+				$("#field-deportado").change( function () {
+					if($("#field-deportado").val() == 1) {
+						$("#momento_deportado_field_box").show();
+						$("#momento_deportado_field_box").show();
+					} else {
+						$("#momento_deportado_field_box").hide();
+						$("#momento_deportado_field_box").hide();
+					}
+				});
+				
+				/*Familiar separacion*/
+				$("#familiar_separado_field_box").css("margin-left", "50px");
+				$("#familiar_separado_field_box").hide();
+				$("#situacion_familiar_field_box").css("margin-left", "50px");
+				$("#situacion_familiar_field_box").hide();
+		
+				$("#field-separacion_familiar").change( function () {
+					if($("#field-separacion_familiar").val() == 1) {
+						$("#familiar_separado_field_box").show();
+						$("#familiar_separado_field_box").show();
+						
+						$("#situacion_familiar_field_box").show();
+						$("#situacion_familiar_field_box").show();
+					} else {
+						$("#situacion_familiar_field_box").hide();
+						$("#situacion_familiar_field_box").hide();
+						
+						$("#familiar_separado_field_box").hide();
+						$("#familiar_separado_field_box").hide();
+					}
+				});
+				
+				/*Migrante - Pueblo indigena*/
+				$("#nombre_pueblo_indigena_field_box").css("margin-left", "50px");
+				$("#nombre_pueblo_indigena_field_box").hide();
+				$("#espanol_field_box").css("margin-left", "50px");
+				$("#espanol_field_box").hide();
+		
+				$("#field-pueblo_indigena").change( function () {
+					if($("#field-pueblo_indigena").val() == 1) {
+						$("#nombre_pueblo_indigena_field_box").show();
+						$("#nombre_pueblo_indigena_field_box").show();
+						
+						$("#espanol_field_box").show();
+						$("#espanol_field_box").show();
+					} else {
+						$("#nombre_pueblo_indigena_field_box").hide();
+						$("#nombre_pueblo_indigena_field_box").hide();
+						
+						$("#espanol_field_box").hide();
+						$("#espanol_field_box").hide();
+					}
+				});
+			<?php } ?>
+			
+			<?php if($this->uri->segment(3) == "read") { ?>
+				/*Mostrando 1 y 2 - Denuncias*/
+				$(".readonly_label").css("margin-left", "30px");
+			
+				if($("#field-separacion_familiar").text() == "1") {
+					$("#field-separacion_familiar").text("Si");
+				} else {
+					$("#field-separacion_familiar").text("No");
+				}
+				
+				if($("#field-deportado").text() == "1") {
+					$("#field-deportado").text("Si");
+				} else {
+					$("#field-deportado").text("No");
+				}
+				
+				if($("#field-viaja_solo").text() == "1") {
+					$("#field-viaja_solo").text("Si");
+				} else {
+					$("#field-viaja_solo").text("No");
+				}
+				
+				if($("#field-coyote_guia").text() == "1") {
+					$("#field-coyote_guia").text("Si");
+				} else {
+					$("#field-coyote_guia").text("No");
+				}
+
+				if($("#field-dano_autoridad").text() == "1") {
+					$("#field-dano_autoridad").text("Si");
+				} else {
+					$("#field-dano_autoridad").text("No");
+				}
+				
+				/*Migrantes*/
+				if($("#field-pueblo_indigena").text() == "1") {
+					$("#field-pueblo_indigena").text("Si");
+				} else if($("#field-pueblo_indigena").text() == "2") {
+					$("#field-pueblo_indigena").text("No");
+				}
+				
+				if($("#field-espanol").text() == "1") {
+					$("#field-espanol").text("Si");
+				} else if($("#field-espanol").text() == "2") {
+					$("#field-espanol").text("No");
+				}
+			<?php } ?>
+		});
+    </script>
+</body>
+</html>
