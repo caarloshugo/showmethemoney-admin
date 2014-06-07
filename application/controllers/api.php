f986c56d4761471e36bb3b41c9d78686255f5f9a
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2014-06-07 14:25:43 -0500

    api

---
+++ application/controllers/api.php
@@ -0,0 +1,28 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->load->database();
+		
+		//Helpers
+		$this->load->helper('url');
+		$this->load->helper('date');
+	}
+
+	
+	/*Metodo facturas*/
+	public function facturas() {
+		$this->load->model('showme_model');
+		$user = $this->showme_model->isUser(trim(str_replace("'","",$_POST["email"])), md5($_POST["pwd"]));
+	}
+	
+	/*metodo index - redirect a denuncias*/
+	public function index() {
+		header('Location: ' . site_url('admin/facturas'));
+		
+		return false;
+	}
+}
